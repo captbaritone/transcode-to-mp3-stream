3745506ae3da76062bce7716b583237849cad7fd
Jordan Eldredge <jordaneldredge@gmail.com>
2014-02-26 20:36:29 -0800

    Actually test ffmpeg. Requires ffprobe json output

--- tests/TranscoderTest.php
+++ tests/TranscoderTest.php
@@ -15,11 +15,33 @@ class TranscoderTest extends \PHPUnit_Framework_TestCase
     {
         $transcoder = new Transcoder();
 
-        $command = $transcoder->command('testfile.flac', 'KBPS', 'start', 'end');
+        $command = $transcoder->command('testfile.flac', 'KBPS', 'start', 'duration');
 
-        $expected = "ffmpeg -ss 'start' -t 'end' -i 'testfile.flac' -ab 'KBPSk' -minrate 'KBPSk' -maxrate 'KBPSk' -bufsize 64k -f mp3 -map_metadata -1  - 2>/dev/null";
+        $expected = "ffmpeg -ss 'start' -t 'duration' -i 'testfile.flac' -ab 'KBPSk' -minrate 'KBPSk' -maxrate 'KBPSk' -bufsize 64k -f mp3 -map_metadata -1  - 2>/dev/null";
 
         $this->assertEquals($command, $expected);
+    }
+
+
+    public function testFFmpegCommandWorks()
+    {
+        $start = 2;
+        $duration = 5;
+        $kbps = 192;
+        $testFilename = 'phpunit_test.mp3';
+
+        $transcoder = new Transcoder();
+
+        $command = $transcoder->command('example.flac', $kbps, $start, $duration);
+        $command .= " > {$testFilename}";
+        shell_exec($command);
+
+        $data = json_decode(shell_exec("ffprobe -v quiet -print_format json -show_format -show_streams {$testFilename}"));
+
+        $this->assertEquals($duration, round($data->format->duration, 1));
+        $this->assertEquals('mp3', $data->format->format_name);
+        $this->assertEquals($kbps * 1000, $data->streams[0]->bit_rate);
+        unlink($testFilename);
     }
 
 }
